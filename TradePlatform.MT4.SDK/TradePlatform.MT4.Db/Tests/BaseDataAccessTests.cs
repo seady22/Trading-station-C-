df3dcf5fb12d5adf6789a6aea77794f94e219360
svyat <svyatoslav.kravchenko@gmail.com>
2013-10-31 22:59:23 +0200

    Add empty unit test

---
+++ TradePlatform.MT4.SDK/TradePlatform.MT4.Db/Tests/BaseDataAccessTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MbUnit.Framework;
+
+namespace TradePlatform.MT4.Db.Tests
+{
+    [TestFixture, Parallelizable(TestScope.All)]
+    public class BaseDataAccessTests
+    {
+        [Test]
+        public void Init()
+        {
+           
+        }
+    }
+}
