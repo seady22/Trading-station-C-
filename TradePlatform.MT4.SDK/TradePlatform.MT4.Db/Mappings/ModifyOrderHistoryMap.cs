70121996ec0491f3ca6dd51b60568469df459d62
Svyat Kravchenko <svyatoslav.kravchenko@wonga.com>
2013-12-16 20:26:04 +0000

    Fix issue with db mapping

--- TradePlatform.MT4.SDK/TradePlatform.MT4.Db/Mappings/ModifyOrderHistoryMap.cs
+++ TradePlatform.MT4.SDK/TradePlatform.MT4.Db/Mappings/ModifyOrderHistoryMap.cs
@@ -3,17 +3,15 @@ using TradePlatform.MT4.Db.Entities;
 
 namespace TradePlatform.MT4.Db.Mappings
 {
-    public class ModifyOrderHistoryMap : ClassMap<ModifyOrderHistory>
+    public class ModifyOrderHistoryMap :ClassMap<ModifyOrderHistory>
     {
         public ModifyOrderHistoryMap()
         {
-            Table("ExpertDetails");
-            Schema("ModifyOrderHistory");
+            Schema("ExpertDetails");
+            Table("ModifyOrderHistory");
 
             Id(x => x.Id);
             Map(x => x.CreatedOn);
-            Map(x => x.OrderId);
-            Map(x => x.OrderId);
             Map(x => x.Points);
             Map(x => x.UpdatedStopLoss);
             Map(x => x.UpdatedTakeProfit);
