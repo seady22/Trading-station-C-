c6ff448ca19dde37460feb8f09d8e35a20d53901
svyat <svyatoslav.kravchenko@gmail.com>
2013-12-30 19:54:23 +0200

    Make minor change to SimpleMaScalperExpert

--- TradePlatform.MT4.SDK/TradePlatform.MT4.SDK.Library/Experts/SimpleMaScalperExpert/SimpleMaScalperExpert.cs
+++ TradePlatform.MT4.SDK/TradePlatform.MT4.SDK.Library/Experts/SimpleMaScalperExpert/SimpleMaScalperExpert.cs
@@ -12,19 +12,31 @@ namespace TradePlatform.MT4.SDK.Library.Experts.SimpleMaScalperExpert
         {
             var result = TREND_TYPE.OTHER;
             var timeFrame = GetCurrentTimeFrame();
-            var sma200Price = TechnicalIndicatiorsWrapper.iMA(this, _symbol, timeFrame, 200, 0, MA_METHOD.MODE_SMA, APPLY_PRICE.PRICE_CLOSE, 0);
+            var sma200M5Price = TechnicalIndicatiorsWrapper.iMA(this, _symbol, timeFrame, 200, 0, MA_METHOD.MODE_SMA, APPLY_PRICE.PRICE_CLOSE, 0);
             var askPrice = PredefinedVariablesWrapper.Ask(this);
             var bidPrice = PredefinedVariablesWrapper.Bid(this);
 
-            if (askPrice >= sma200Price && bidPrice >= sma200Price)
+            if (askPrice >= sma200M5Price && bidPrice >= sma200M5Price)
             {
                 result = TREND_TYPE.ASC;
             }
 
-            if (askPrice <= sma200Price && bidPrice <= sma200Price)
+            if (askPrice <= sma200M5Price && bidPrice <= sma200M5Price)
             {
                 result = TREND_TYPE.DESC;
             }
+
+            var sma200H1Price = TechnicalIndicatiorsWrapper.iMA(this, _symbol, TIME_FRAME.PERIOD_H1, 200, 0, MA_METHOD.MODE_SMA, APPLY_PRICE.PRICE_CLOSE, 0);
+            if (askPrice >= sma200H1Price && bidPrice >= sma200H1Price && result == TREND_TYPE.ASC)
+            {
+                result = TREND_TYPE.ASC;
+            }
+
+            if (askPrice <= sma200H1Price && bidPrice <= sma200H1Price && result == TREND_TYPE.DESC)
+            {
+                result = TREND_TYPE.DESC;
+            }
+
             if (result == TREND_TYPE.OTHER)
             {
                 throw new Exception("Problem with declaring trend type");
