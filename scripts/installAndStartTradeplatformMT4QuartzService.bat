d8212de51af7dd4099c17b4ded669a15c2a69125
svyat <svyatoslav.kravchenko@gmail.com>
2014-01-09 23:02:29 +0200

    Remove unused sc files

--- scripts/installAndStartTradeplatformMT4QuartzService.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-cd deploy
-cd Quartz
-installutil.exe TradePlatform.MT4.SDK.QuartzService.exe
-SC START TradePlatform.QuartzService
--- scripts/stopAndDeleteTradeplatformMT4QuartzService.bat
+++
@@ -1,3 +0,0 @@
-SC STOP TradePlatform.QuartzService
-SC DELETE TradePlatform.QuartzService
-TASKKILL /IM TradePlatform.MT4.SDK.QuartzService.exe /f
